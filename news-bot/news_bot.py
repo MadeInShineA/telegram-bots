1a847518876b544c38f3689f8852216462a333ff
MadeInShineA <olivier.amacker@netplus.ch>
2023-05-01 23:45:49 +0200

    Upgrade to bypass the 10 results limit per request with News Data + added the time message

--- news-bot/news_bot.py
+++ news-bot/news_bot.py
@@ -4,6 +4,7 @@ import json
 from bs4 import BeautifulSoup
 from bs4.element import Comment
 from dotenv import load_dotenv
+import datetime
 import os
 
 load_dotenv()
@@ -28,14 +29,22 @@ avoided_tags = ['style', 'script', 'head', 'title', 'meta', '[document]']
 
 # Define the /news command handler
 def news():
-
+    today = datetime.date.today().strftime('%B %d, %Y')
+    starting_message = f'📰 *{today}\'s News* 📰'
+    send_message(starting_message,markdown=True)
     # Fetch the latest news headlines from the News API
-    url = f'https://newsdata.io/api/1/news?apikey={news_data_key}&category=science,technology&language=en&domain=phys,wired,sciencedaily,sciencealert'
-    response = requests.get(url)
-    data = json.loads(response.text)
+    # Uses a for loop to bypass the 10 results per request
+    data = []
+    for news_source in source_content_dict.keys():
+        url = f'https://newsdata.io/api/1/news?apikey={news_data_key}&category=science,technology&language=en&domain={news_source}'
+        responses = requests.get(url)
+        responses = json.loads(responses.text)
 
-    data = [data_with_content for data_with_content in data['results'] if
-            data_with_content.get('link')]
+        for response in responses['results']:
+            data.append(response)
+
+    data = [data_with_link for data_with_link in data if
+            data_with_link.get('link')]
 
     # Summarize the news articles using the TextGear API
     for article in data:
@@ -43,11 +52,13 @@ def news():
         url = article['link']
         content = extract_content(url, source)
         summary = summarize_text(content)
-        message = f"{article['title']}\n\n{summary}\n\n{url}"
+        message = f'{article["title"]}\n\n{summary}\n\n{url}'
+
         send_message(message)
 
 
 # Define the function to extract the content of an article
+
 def extract_content(url, source):
     element_type, element_class = list(source_content_dict[source].items())[0]
 
@@ -56,12 +67,12 @@ def extract_content(url, source):
     }
     website = requests.get(url, headers=headers)
     soup = BeautifulSoup(website.text, 'html.parser')
-    # find the div element with class "post-content"
+    # Find the element with corresponding class
     news_content = soup.find(element_type, class_=element_class)
     news_text = news_content.findAll(string=True)
 
     news_text = filter(tag_visible, news_text)
-    content = u" ".join(t.strip() for t in news_text)
+    content = u' '.join(t.strip() for t in news_text)
 
     return content
 
@@ -75,12 +86,16 @@ def summarize_text(text):
 
 
 # Define the function to send a message via the Telegram Bot API
-def send_message(message):
-    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+def send_message(message, markdown=False):
+    url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
+
     payload = {
-        "chat_id": bot_chatID,
-        "text": message
+        'chat_id': bot_chatID,
+        'text': message,
     }
+
+    if markdown:
+        payload['parse_mode'] = 'MarkdownV2'
     requests.post(url, json=payload)
 
 
